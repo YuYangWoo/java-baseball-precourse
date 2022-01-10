2fcd631227e13a22bb80b89bfb49a13dbcbb4e80
a1101702 <yuyw0712@naver.com>
2022-01-10 14:06:03 +0900

    feat : BaseBall게임을 시작하는 클래스 구현 사용자의 입력을 받아 게임을 시작하는 클래스 구현

---
+++ src/main/java/baseball/baseball/BaseballGame.java
@@ -0,0 +1,35 @@
+package baseball.baseball;
+
+import baseball.Core.BaseBallPlaying;
+import baseball.ui.Input;
+import baseball.ui.Output;
+import nextstep.utils.IsValidInput;
+
+public class BaseballGame {
+    String computerNumber = Computer.getComputerNumber();
+    String userNumber;
+
+    public void playBaseBall() {
+        Input input = new Input();
+        BaseBallPlaying baseBallPlaying = new BaseBallPlaying();
+        do {
+            try {
+                IsValidInput isValidInput = new IsValidInput(input.inputYourNumber());
+                userNumber = isValidInput.isChecked();
+                baseBallPlaying.getGameScore(userNumber, computerNumber);
+            } catch (IllegalArgumentException e) {
+                Output.printExceptionMessage(e.getMessage());
+            }
+        } while(checkEqual());
+    }
+
+    public boolean checkEqual() {
+        if(computerNumber.equals(userNumber)) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+
+}
