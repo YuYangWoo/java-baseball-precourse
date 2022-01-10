6205416cbab67ba3ce70a0950eda4ea9d2f8d85b
a1101702 <yuyw0712@naver.com>
2022-01-10 14:08:57 +0900

    feat : BaseBallPlaying 게임의 힌트(스코어)를 얻는 클래스 구현 스트라이크, 볼, 낫싱의 결과를 얻는 클래스 구현

---
+++ src/main/java/baseball/Core/BaseBallPlaying.java
@@ -0,0 +1,37 @@
+package baseball.Core;
+
+import baseball.ui.Output;
+
+public class BaseBallPlaying {
+
+    public String getGameScore(String userNumber, String computerNumber) {
+        int strike = countStrike(userNumber, computerNumber);
+        int ball = countBall(userNumber, computerNumber);
+        Result result = new Result(strike, ball);
+        Output.printBaseballGaemResult(result.getResult());
+        return result.getResult();
+    }
+
+    public int countStrike(String input, String randomNumber) {
+        int strikeCount = 0;
+        for(int i = 0; i < input.length(); i++) {
+            if(input.charAt(i) == randomNumber.charAt(i)) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
+
+    public int countBall(String input, String randomNumber) {
+        int ballCount = 0;
+        for(int i = 0 ; i < input.length(); i++) {
+            for(int j = 0; j < randomNumber.length(); j++) {
+                if(i != j && input.charAt(i) == randomNumber.charAt(j)) {
+                    ballCount++;
+                }
+            }
+        }
+        return ballCount;
+    }
+
+}
