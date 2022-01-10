ccc2208d96d5f228463abfb63c6c2ed069027c7d
a1101702 <yuyw0712@naver.com>
2022-01-10 11:03:37 +0900

    feat : Applicaton GameLauncher 구현 application GameLauncher 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+
+import java.io.IOException;
+import nextstep.utils.Console;
+import nextstep.utils.Randoms;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO 숫자 야구 게임 구현
+        Launcher launcher = new Launcher();
+        launcher.start();
     }
 }
